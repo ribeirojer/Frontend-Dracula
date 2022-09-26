5b0e5fd7906a5ed127b9a758934f91ec0cde1e10
José Eduardo Ribeiro <100392655+ribeirojer@users.noreply.github.com>
2022-09-25 22:34:27 -0300

    Update User.tsx

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -5,6 +5,7 @@ type Props = {}
 const User = (props: Props) => {
   return (
     <div>
+      <img src=""></img>
       <p>exibir informações do usuário, endereço e tudo mais</p>
       <p>informações podem ser editadas</p>
     </div>
