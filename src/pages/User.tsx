edd234723bcae2ad2142de9dad6bc8138d61a232
José Eduardo Ribeiro <100392655+ribeirojer@users.noreply.github.com>
2022-09-29 23:20:04 -0300

    Update User.tsx

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import image3 from '../assets/corte-e-gravacao-laser-mdf-e-acrilico-01.jpg'
 
 type Props = {}
 
 const User = (props: Props) => {
   return (
     <div>
-      <img src=""></img>
+      <img src={image3}></img>
       <p>exibir informações do usuário, endereço e tudo mais</p>
       <p>informações podem ser editadas</p>
     </div>
