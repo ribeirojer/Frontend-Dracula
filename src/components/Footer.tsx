b768e5ebafa6e0888418b0b0ee5ce1b838bb51b4
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2023-03-12 21:21:32 -0300

    edit footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,18 @@
-import { EnvelopeSimple, FacebookLogo, InstagramLogo, LinkedinLogo, MapPin, Phone, PinterestLogo, TwitterLogo } from 'phosphor-react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components'
-import image from '../assets/woman.png'
+import {
+  EnvelopeSimple,
+  FacebookLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  MapPin,
+  Phone,
+  PinterestLogo,
+  TwitterLogo,
+} from "phosphor-react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import image from "../assets/woman.png";
 
-type Props = {}
+type Props = {};
 
 const Wrapper = styled.footer`
   background-color: var(--cor-A);
@@ -15,141 +24,262 @@ const Wrapper = styled.footer`
   height: 432px;
 `;
 const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  div {
+    width: 33%;
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
     align-items: center;
-    div {
-      width: 33%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    }
+  }
 `;
 const Contacts = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    margin-top: 0.7rem;
     display: flex;
-    flex-direction: column;
     align-items: center;
-    p {
-      margin-top: 0.7rem;
-      display: flex;
-      align-items: center;
-    }
-    p svg {
-      margin-right: 5px;
-    }
+  }
+  p svg {
+    margin-right: 5px;
+  }
 `;
 const Company = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    margin-bottom: 0.8rem;
+  }
+  div {
+    width: 100%;
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    p {
-      margin-bottom: 0.8rem;
-    }
-    div {
-      width: 100%;
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-    }
-    div a {
-      color: var(--cor-C);
-      margin: 0 0.5rem;
-      cursor: pointer;
-      transition: 0.3s;
-    }
-    div a:hover {
-      color: var(--cor-E);
-    }
+    flex-direction: row;
+    justify-content: center;
+  }
+  div a {
+    color: var(--cor-C);
+    margin: 0 0.5rem;
+    cursor: pointer;
+    transition: 0.3s;
+  }
+  div a:hover {
+    color: var(--cor-E);
+  }
 `;
 const Logo = styled.span`
-    font-size: 2rem;
-    font-family: 'Dancing Script';
-    font-weight: 700;
-    margin-bottom: 1rem;
-    a {
-        display: flex;
-        align-items: center;
-        color: var(--cor-C);
-        text-decoration: none;
-    }
-    img {
-        border: 1px solid #fff;
-        border-radius: 50%;
-        height: 40px;
-        margin-right: 0.5rem;
-    }
-    
+  font-size: 2rem;
+  font-family: "Dancing Script";
+  font-weight: 700;
+  margin-bottom: 1rem;
+  a {
+    display: flex;
+    align-items: center;
+    color: var(--cor-C);
+    text-decoration: none;
+  }
+  img {
+    border: 1px solid #fff;
+    border-radius: 50%;
+    height: 40px;
+    margin-right: 0.5rem;
+  }
 `;
 const Hours = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    h3 {
-      font-size: 28px;
-      font-family: 'Dancing Script';
-      font-weight: 700;
-      margin-bottom: 1rem;
-    }
-    p {
-      margin-top: 0.7rem;
-    }
-    p span {
-      margin: 0 0.5rem;
-    }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h3 {
+    font-size: 28px;
+    font-family: "Dancing Script";
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+  p {
+    margin-top: 0.7rem;
+  }
+  p span {
+    margin: 0 0.5rem;
+  }
 `;
 const Last = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 5rem;
-    p {
-      margin-bottom: 2rem;
-    }
-    p span {
-      color: #f00;
-    }
-    a {
-      color: var(--cor-C);
-      text-decoration: none;
-    }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 5rem;
+  p {
+    margin-bottom: 2rem;
+  }
+  p span {
+    color: #f00;
+  }
+  a {
+    color: var(--cor-C);
+    text-decoration: none;
+  }
 `;
 
 const Footer = (props: Props) => {
   return (
-    <Wrapper>
-      <FooterWrapper>
-        <Contacts>
-          <h3>Entre em Contato</h3>
-          <p><MapPin size={20} weight="fill"/> Rua Paulo Malschitzki, 200, Joinville - SC</p>
-          <p><Phone size={20} weight="fill"/> (12) 98158-0992</p>
-          <p><EnvelopeSimple size={20} weight="fill"/> eduardojerbr@gmail.com</p>
-        </Contacts>
-        <Company>          
-          <Logo>
-              <Link to={"/"}><img src={image} alt="" />Maria</Link>
-          </Logo>
-          <p>Artigos personalizados em MDF e acrílico, corte e </p>
-          <p>impresão a laser, presentes e muito mais!</p>
-          <div>
-            <a href='https://www.facebook.com' target={'_blank'}><FacebookLogo size={32} /></a>
-            <a href='https://www.twitter.com' target={'_blank'}><TwitterLogo size={32} /></a>
-            <a href='https://www.linkedin.com' target={'_blank'}><LinkedinLogo size={32} /></a>
-            <a href='https://www.instagram.com' target={'_blank'}><InstagramLogo size={32} /></a>
-            <a href='https://www.pinterest.com' target={'_blank'}><PinterestLogo size={32} /></a>
+    <footer id="footer">
+      <div className="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 col-xs-6">
+              <div className="footer">
+                <h3 className="footer-title">About Us</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut.
+                </p>
+                <ul className="footer-links">
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-map-marker"></i>1734 Stonecoal Road
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-phone"></i>+021-95-51-84
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-envelope-o"></i>email@email.com
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-md-3 col-xs-6">
+              <div className="footer">
+                <h3 className="footer-title">Categories</h3>
+                <ul className="footer-links">
+                  <li>
+                    <a href="#">Hot deals</a>
+                  </li>
+                  <li>
+                    <a href="#">Laptops</a>
+                  </li>
+                  <li>
+                    <a href="#">Smartphones</a>
+                  </li>
+                  <li>
+                    <a href="#">Cameras</a>
+                  </li>
+                  <li>
+                    <a href="#">Accessories</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="clearfix visible-xs"></div>
+
+            <div className="col-md-3 col-xs-6">
+              <div className="footer">
+                <h3 className="footer-title">Information</h3>
+                <ul className="footer-links">
+                  <li>
+                    <a href="#">About Us</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact Us</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a href="#">Orders and Returns</a>
+                  </li>
+                  <li>
+                    <a href="#">Terms & Conditions</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-md-3 col-xs-6">
+              <div className="footer">
+                <h3 className="footer-title">Service</h3>
+                <ul className="footer-links">
+                  <li>
+                    <a href="#">My Account</a>
+                  </li>
+                  <li>
+                    <a href="#">View Cart</a>
+                  </li>
+                  <li>
+                    <a href="#">Wishlist</a>
+                  </li>
+                  <li>
+                    <a href="#">Track My Order</a>
+                  </li>
+                  <li>
+                    <a href="#">Help</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-        </Company>
-        <Hours>
-          <h3>Atendimento</h3>
-          <p>Segunda à Sábado</p>
-          <p><span>08:00 </span> às <span> 17:00</span></p>
-        </Hours>
-      </FooterWrapper>
-      <Last>
-        <p>&copy; Princesa Maria - Todos os direitos reservados.</p>
-        <p>Desenvolvido com <span> &#x2764; </span> por <a target={"_blank"} href="http://www.eduardojer.tech">José Eduardo Ribeiro</a></p>
-      </Last>
-    </Wrapper>
-  )
-}
+        </div>
+      </div>
+
+      <div id="bottom-footer" className="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-center">
+              <ul className="footer-payments">
+                <li>
+                  <a href="#">
+                    <i className="fa fa-cc-visa"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-credit-card"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-cc-paypal"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-cc-mastercard"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-cc-discover"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-cc-amex"></i>
+                  </a>
+                </li>
+              </ul>
+              <span className="copyright">
+                Copyright &copy;
+                <script>document.write(new Date().getFullYear());</script> All
+                rights reserved | This template is made with{" "}
+                <i className="fa fa-heart-o" aria-hidden="true"></i> by{" "}
+                <a href="https://colorlib.com" target="_blank">
+                  Colorlib
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
