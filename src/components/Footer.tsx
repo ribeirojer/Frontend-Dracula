22ff50b54bb4123da404a230781b89208cdd35b3
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2023-03-17 23:50:36 -0300

    edit sectionA

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,182 +1,161 @@
-import {
-  EnvelopeSimple,
-  FacebookLogo,
-  InstagramLogo,
-  LinkedinLogo,
-  MapPin,
-  Phone,
-  PinterestLogo,
-  TwitterLogo,
-} from "phosphor-react";
+import { MapPin } from "phosphor-react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import image from "../assets/woman.png";
 
 type Props = {};
 
 const Wrapper = styled.footer`
-  background-color: var(--cor-A);
-  color: var(--cor-C);
+  color: #fff;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
-  padding: 0 8%;
-  height: 432px;
+  .row {
+    background-color: #15161d;
+    div {
+      width: 25%;
+      ul {
+        display: flex;
+        flex-direction: column;
+      }
+    }
+    display: flex;
+  }
 `;
 
 const Footer = (props: Props) => {
   return (
-    <footer id="footer">
-      <div className="section">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3 col-xs-6">
-              <div className="footer">
-                <h3 className="footer-title">Sobre a Empresa</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut.
-                </p>
-                <ul className="footer-links">
-                  <li>
-                    <a href="#">
-                      <i className="fa fa-map-marker"></i>Rua Paulo Malschitzki, 240, Joinville - SC
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i className="fa fa-phone"></i>+55 (12) 98158-0992
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i className="fa fa-envelope-o"></i>contato@electro.com
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+    <Wrapper>
+      <div className="row">
+        <div>
+          <h3 className="footer-title">Sobre a Empresa</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut.
+          </p>
+          <ul className="footer-links">
+            <li>
+              <Link to={"/"}>
+                <MapPin></MapPin> Rua Paulo Malschitzki, 240, Joinville - SC
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <i className="fa fa-phone"></i>+55 (12) 98158-0992
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <i className="fa fa-envelope-o"></i>contato@electro.com
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-            <div className="col-md-3 col-xs-6">
-              <div className="footer">
-                <h3 className="footer-title">Categorias</h3>
-                <ul className="footer-links">
-                  <li>
-                    <a href="#">Promoção</a>
-                  </li>
-                  <li>
-                    <a href="#">Laptops</a>
-                  </li>
-                  <li>
-                    <a href="#">Smartphones</a>
-                  </li>
-                  <li>
-                    <a href="#">Câmeras</a>
-                  </li>
-                  <li>
-                    <a href="#">Acessórios</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+        <div>
+          <h3 className="footer-title">Categorias</h3>
+          <ul className="footer-links">
+            <li>
+              <Link to={"/"}>Promoção</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Laptops</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Smartphones</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Câmeras</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Acessórios</Link>
+            </li>
+          </ul>
+        </div>
 
-            <div className="clearfix visible-xs"></div>
+        <div>
+          <h3 className="footer-title">Informações</h3>
+          <ul className="footer-links">
+            <li>
+              <Link to={"/"}>Sobre a empresa</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Entre em contato</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Política de Privacidade</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Ordens e Retornos</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Termos e Condições</Link>
+            </li>
+          </ul>
+        </div>
 
-            <div className="col-md-3 col-xs-6">
-              <div className="footer">
-                <h3 className="footer-title">Informações</h3>
-                <ul className="footer-links">
-                  <li>
-                    <a href="#">Sobre a empresa</a>
-                  </li>
-                  <li>
-                    <a href="#">Entre em contato</a>
-                  </li>
-                  <li>
-                    <a href="#">Política de Privacidade</a>
-                  </li>
-                  <li>
-                    <a href="#">Ordens e Retornos</a>
-                  </li>
-                  <li>
-                    <a href="#">Termos e Condições</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="col-md-3 col-xs-6">
-              <div className="footer">
-                <h3 className="footer-title">Serviços</h3>
-                <ul className="footer-links">
-                  <li>
-                    <a href="#">Minha Conta</a>
-                  </li>
-                  <li>
-                    <a href="#">Ver Carrinho</a>
-                  </li>
-                  <li>
-                    <a href="#">Lista de Desejos</a>
-                  </li>
-                  <li>
-                    <a href="#">Finalizar minha ordem</a>
-                  </li>
-                  <li>
-                    <a href="#">Ajuda</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+        <div>
+          <h3 className="footer-title">Serviços</h3>
+          <ul className="footer-links">
+            <li>
+              <Link to={"/"}>Minha Conta</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Ver Carrinho</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Lista de Desejos</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Finalizar minha ordem</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Ajuda</Link>
+            </li>
+          </ul>
         </div>
       </div>
 
-      <div id="bottom-footer" className="section">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 text-center">
-              <ul className="footer-payments">
-                <li>
-                  <a href="#">
-                    <i className="fa fa-cc-visa"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-credit-card"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-cc-paypal"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-cc-mastercard"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-cc-discover"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-cc-amex"></i>
-                  </a>
-                </li>
-              </ul>
-              <span className="copyright">
-                Copyright &copy;
-                <script>document.write(new Date().getFullYear());</script> Todos
-                os direitos reservados
-              </span>
-            </div>
-          </div>
+      <div className="container">
+        <div className="col-md-12 text-center">
+          <ul className="footer-payments">
+            <li>
+              <Link to={"/"}>
+                <i className="fa fa-cc-visa"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <i className="fa fa-credit-card"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <i className="fa fa-cc-paypal"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <i className="fa fa-cc-mastercard"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <i className="fa fa-cc-discover"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <i className="fa fa-cc-amex"></i>
+              </Link>
+            </li>
+          </ul>
+          <span className="copyright">
+            Copyright &copy;
+            <script>document.write(new Date().getFullYear());</script> Todos os
+            direitos reservados
+          </span>
         </div>
       </div>
-    </footer>
+    </Wrapper>
   );
 };
 
--- src/components/SectionA.tsx
+++ src/components/SectionA.tsx
@@ -1,62 +1,83 @@
-import React from "react";
+import shop01 from "../assets/shop01.png";
+import shop02 from "../assets/shop03.png";
+import shop03 from "../assets/shop02.png";
+import styled from "styled-components";
 
 type Props = {};
 
+const WrapperSectionA = styled.section`
+  display: flex;
+  justify-content: center;
+  .shop {
+    width: 33%;
+    position: relative;
+    img {
+      width: 100%;
+    }
+    .shop-body {
+      position: absolute;
+      top: 0;
+      width: 75%;
+      padding: 30px;
+      z-index: 10;
+    }
+  }
+  .shop:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0px;
+    width: 60%;
+    background: #d10024;
+    opacity: 0.9;
+    transform: skewX(-45deg);
+  }
+`;
+
 const SectionA = (props: Props) => {
   return (
-    <div className="section">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4 col-xs-6">
-            <div className="shop">
-              <div className="shop-img">
-                <img src="./img/shop01.png" alt="" />
-              </div>
-              <div className="shop-body">
-                <h3>
-                  Laptop<br></br>Collection
-                </h3>
-                <a href="#" className="cta-btn">
-                  Shop now <i className="fa fa-arrow-circle-right"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4 col-xs-6">
-            <div className="shop">
-              <div className="shop-img">
-                <img src="./img/shop03.png" alt="" />
-              </div>
-              <div className="shop-body">
-                <h3>
-                  Accessories<br></br>Collection
-                </h3>
-                <a href="#" className="cta-btn">
-                  Shop now <i className="fa fa-arrow-circle-right"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4 col-xs-6">
-            <div className="shop">
-              <div className="shop-img">
-                <img src="./img/shop02.png" alt="" />
-              </div>
-              <div className="shop-body">
-                <h3>
-                  Cameras<br></br>Collection
-                </h3>
-                <a href="#" className="cta-btn">
-                  Shop now <i className="fa fa-arrow-circle-right"></i>
-                </a>
-              </div>
-            </div>
-          </div>
+    <WrapperSectionA>
+      <div className="shop">
+        <img src={shop01} alt="" />
+        <div className="shop-body">
+          <h3>
+            Laptop<br></br>Collection
+          </h3>
+          <a href="#" className="cta-btn">
+            Shop now <i className="fa fa-arrow-circle-right"></i>
+          </a>
         </div>
       </div>
-    </div>
+
+      <div className="shop">
+        <div className="shop-img">
+          <img src={shop02} alt="" />
+        </div>
+        <div className="shop-body">
+          <h3>
+            Accessories<br></br>Collection
+          </h3>
+          <a href="#" className="cta-btn">
+            Shop now <i className="fa fa-arrow-circle-right"></i>
+          </a>
+        </div>
+      </div>
+
+      <div className="shop">
+        <div className="shop-img">
+          <img src={shop03} alt="" />
+        </div>
+        <div className="shop-body">
+          <h3>
+            Cameras<br></br>Collection
+          </h3>
+          <a href="#" className="cta-btn">
+            Shop now <i className="fa fa-arrow-circle-right"></i>
+          </a>
+        </div>
+      </div>
+    </WrapperSectionA>
   );
 };
 
